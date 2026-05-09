2890bd41ac7ec5a774aaab370887e88af075d013
Goendyr <jacob.schlachte@gmail.com>
2026-05-09 17:53:26 +0200

    baby wiggles again

--- baby.gd
+++ baby.gd
@@ -1,7 +1,8 @@
 extends Node3D
 
 @export
-var crawl_speed = 3.3
+var crawl_speed = 5.3
+var wiggle_duration = 0.5
 
 var crawl_l: Tween
 var crawl_r: Tween
@@ -20,7 +21,6 @@ var rng = RandomNumberGenerator.new()
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	
 	limbs_left = $body/limbs_left
 	limbs_right = $body/limbs_right
 	body = $"body"
@@ -30,45 +30,47 @@ func _ready() -> void:
 	crawl_l = get_tree().create_tween().set_loops() 
 	crawl_l.set_parallel(false)
 	crawl_l.tween_property(limbs_left, "position", \
-	limbs_left.position + Vector3(-0.13, 0.0, 0.0), crawl_speed) \
+	limbs_left.position + Vector3(-0.13, 0.0, 0.0), wiggle_duration) \
 	.set_trans(Tween.TRANS_QUAD)
 	crawl_l.tween_property(limbs_left, "position", \
-	limbs_left.position +Vector3(0.0, 0.0, 0.0), crawl_speed) \
+	limbs_left.position +Vector3(0.0, 0.0, 0.0), wiggle_duration) \
 	.set_trans(Tween.TRANS_QUAD)
 	
 	crawl_r = get_tree().create_tween().set_loops() 
 	crawl_r.set_parallel(false)
 	crawl_r.tween_property(limbs_right, "position", \
-	limbs_right.position +Vector3(0.13, 0.0, 0.0), crawl_speed) \
+	limbs_right.position +Vector3(0.13, 0.0, 0.0), wiggle_duration) \
 	.set_trans(Tween.TRANS_QUAD)
 	crawl_r.tween_property(limbs_right, "position", \
-	limbs_right.position +Vector3(0.0, 0.0, 0.0), crawl_speed) \
+	limbs_right.position +Vector3(0.0, 0.0, 0.0), wiggle_duration) \
 	.set_trans(Tween.TRANS_QUAD)
 	
 	# body wiggle
 	wiggle = get_tree().create_tween().set_loops()
 	wiggle.tween_property(body, "rotation", \
-	Vector3(0, deg_to_rad(-3), 0), crawl_speed) \
+	Vector3(0, deg_to_rad(-3), 0), wiggle_duration) \
 	.as_relative().set_trans(Tween.TRANS_QUAD)
 	wiggle.tween_property(body, "rotation", \
-	Vector3(0, deg_to_rad(3), 0), crawl_speed)\
+	Vector3(0, deg_to_rad(3), 0), wiggle_duration)\
 	.as_relative().set_trans(Tween.TRANS_QUAD)
 
 	gen_set_crawl_dir()
 	
 func _process(delta: float) -> void:
+	if not body.visible:
+		return
 	# crawl
 	position += crawl_dir.normalized() * crawl_speed * delta
 	# turn
 	if crawl_dir != Vector3.ZERO:
-		var target_basis = Basis.looking_at(crawl_dir.rotated(Vector3.UP, -55))
+		var target_basis = Basis.looking_at(crawl_dir.rotated(Vector3.UP, deg_to_rad(90)))
 		basis = basis.slerp(target_basis, delta * turn_speed)
 
 func gen_set_crawl_dir() -> void:
 	crawl_dir = Vector3(\
 	rng.randf_range(-1, 1),\
 	0,
-	rng.randf_range(-1, 1))
+	rng.randf_range(-1, 1)).normalized()
 	#look_at(global_transform.origin + crawl_dir)
 	#var turn = get_tree().create_tween().tween_property($".", rotation, crawl_dir, 1)
 	
@@ -84,5 +86,6 @@ func _on_area_3d_body_entered(body_: Node3D) -> void:
 		body_.jump()
 		$BloodExplosion.splatter()
 		$body.queue_free()
+		$body.hide()
 		$Area3D.queue_free()
 		$AudioStreamPlayer3D.play()
