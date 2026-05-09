7bb9be6a00a3fec4d0ba66f647198895bd79cb8b
Dr0med4r <trompeter-stoll@gmx.de>
2026-05-09 15:16:41 +0200

    disable movement at start

--- src/player/player.gd
+++ src/player/player.gd
@@ -14,7 +14,7 @@ const camera_angle = -16
 @export var boost = 3
 
 # angle_change change per second
-@export var angle_change = deg_to_rad(100)
+@export var angle_change = deg_to_rad(120)
 
 # The downward acceleration when in the air, in meters per second squared.
 @export var fall_acceleration = 9
@@ -22,7 +22,9 @@ const camera_angle = -16
 
 @export var current_direction = Vector3.FORWARD
 
-var will_jump = false
+var _will_jump = false
+
+var _allow_control = false
 
 func _ready():
 	velocity = Vector3.FORWARD
@@ -32,8 +34,10 @@ func _ready():
 
 
 func jump():
-	will_jump = true
+	_will_jump = true
 
+func start_control():
+	_allow_control = true
 
 func _physics_process(delta):
 	
@@ -41,26 +45,27 @@ func _physics_process(delta):
 	var movement_xz = velocity
 	movement_xz.y = 0
 
-	if Input.is_action_pressed("move_backward"):
-		if movement_xz.length() > min_speed:
-			movement_xz -= current_direction * acceleration * delta
-	if Input.is_action_pressed("move_forward"):
-		movement_xz += current_direction * acceleration * delta
-	
-	if Input.is_action_pressed("turn_right"):
-		movement_xz = movement_xz.rotated(Vector3.UP, -angle_change * delta)
-	if Input.is_action_pressed("turn_left"):
-		movement_xz = movement_xz.rotated(Vector3.UP, angle_change * delta)
+	if _allow_control:
+		if Input.is_action_pressed("move_backward"):
+			if movement_xz.length() > min_speed:
+				movement_xz -= current_direction * acceleration * delta
+		if Input.is_action_pressed("move_forward"):
+			movement_xz += current_direction * acceleration * delta
+		
+		if Input.is_action_pressed("turn_right"):
+			movement_xz = movement_xz.rotated(Vector3.UP, -angle_change * delta)
+		if Input.is_action_pressed("turn_left"):
+			movement_xz = movement_xz.rotated(Vector3.UP, angle_change * delta)
 	
 	if not movement_xz.is_equal_approx(Vector3.ZERO):
 		# Setting the basis property will affect the rotation of the node.
 		$Pivot.basis = Basis.looking_at(movement_xz)
 		current_direction = movement_xz.normalized()
 	
-	if will_jump and is_on_floor():
+	if _will_jump and is_on_floor():
 		movement_y += 10
 		movement_xz += current_direction * boost
-		will_jump = false
+		_will_jump = false
 
 
 	
@@ -73,12 +78,6 @@ func _physics_process(delta):
 		movement_y = movement_y - (fall_acceleration * delta)
 
 
-	if not is_on_floor():
-		$Pivot/Camera3D.rotation_degrees = Vector3(clamp(movement_y * 2.5 + camera_angle + 1, -90, camera_angle), 0, 0)
-	else:
-		$Pivot/Camera3D.rotation_degrees = Vector3(clamp($Pivot/Camera3D.rotation_degrees[0] + 1, -90, camera_angle), 0, 0)
-
-	
 	velocity = movement_xz
 	velocity.y  = movement_y
 	# Moving the Character
@@ -87,5 +86,7 @@ func _physics_process(delta):
 		
 
 func _on_bounce_body_entered(body: Node3D) -> void:
+	if body.has_method("start_control"):
+		body.start_control()
 	if body.has_method("jump"):
 		body.jump()
