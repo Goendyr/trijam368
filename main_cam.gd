5a90cc9f1c1ee907c8cdbd789c515800f4e93ac3
Goendyr <jacob.schlachte@gmail.com>
2026-05-09 15:36:47 +0200

    baby has area and is floor aligned

--- main_cam.gd
+++ main_cam.gd
@@ -1,14 +1,15 @@
 extends Camera3D
 
-var start_baby: Node3D
+var start_baby
 var is_game_started = false
 var player: Node3D
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	# TODO change start_baby to select the nearest baby to support restart
-	start_baby = $"../start_baby"
+	start_baby = load("res://baby.tscn")
 	player = $"../FordFocusMesh"
 	player.visible = false
+	#TODO disable following
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
@@ -17,4 +18,5 @@ func _process(delta: float) -> void:
 	if(Input.is_action_just_pressed("start_game") && !is_game_started):
 		is_game_started = true
 		# spawn car
+		
 		# cam zoom out on baby hit
